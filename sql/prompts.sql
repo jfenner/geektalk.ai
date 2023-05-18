523bf22f444777106ea09c38c212119e647be425
Jon Fenner <jon@>
2023-05-17 22:05:01 -0400

    Updates to change the prompt generation method

---
+++ sql/prompts.sql
@@ -0,0 +1,43 @@
+-- SQLite
+CREATE TABLE IF NOT EXISTS prompts (
+            prompt TEXT,
+            used BOOLEAN
+    );
+INSERT INTO prompts (prompt, used) VALUES ("Computer & Technology", false);
+INSERT INTO prompts (prompt, used) VALUES ("Heroes & Villains", false);
+INSERT INTO prompts (prompt, used) VALUES ("The Internet", false);
+INSERT INTO prompts (prompt, used) VALUES ("Doctor Who", false);
+INSERT INTO prompts (prompt, used) VALUES ("Star Trek", false);
+INSERT INTO prompts (prompt, used) VALUES ("Star Wars", false);
+INSERT INTO prompts (prompt, used) VALUES ("Cybersecurity", false);
+INSERT INTO prompts (prompt, used) VALUES ("Cybercrimes", false);
+INSERT INTO prompts (prompt, used) VALUES ("Computer hacking", false);
+INSERT INTO prompts (prompt, used) VALUES ("Phreaking", false);
+INSERT INTO prompts (prompt, used) VALUES ("Stargate", false);
+INSERT INTO prompts (prompt, used) VALUES ("Live Long and Prosper", false);
+INSERT INTO prompts (prompt, used) VALUES ("May the Fourth be With You", false);
+INSERT INTO prompts (prompt, used) VALUES ("Life, The Universe, and Everything", false);
+INSERT INTO prompts (prompt, used) VALUES ("Linux", false);
+INSERT INTO prompts (prompt, used) VALUES ("Windows", false);
+INSERT INTO prompts (prompt, used) VALUES ("Mac Computers", false);
+INSERT INTO prompts (prompt, used) VALUES ("USB", false);
+INSERT INTO prompts (prompt, used) VALUES ("BIOS", false);
+INSERT INTO prompts (prompt, used) VALUES ("BTC Mining", false);
+INSERT INTO prompts (prompt, used) VALUES ("Cryptocurrency", false);
+INSERT INTO prompts (prompt, used) VALUES ("Bitcoin", false);
+INSERT INTO prompts (prompt, used) VALUES ("Blockchain", false);
+INSERT INTO prompts (prompt, used) VALUES ("Warcraft", false);
+INSERT INTO prompts (prompt, used) VALUES ("Fortnite", false);
+INSERT INTO prompts (prompt, used) VALUES ("ComicCon", false);
+INSERT INTO prompts (prompt, used) VALUES ("Klingon Language", false);
+INSERT INTO prompts (prompt, used) VALUES ("One Ring to Rule them All", false);
+INSERT INTO prompts (prompt, used) VALUES ("Proxy Servers", false);
+INSERT INTO prompts (prompt, used) VALUES ("VPN", false);
+INSERT INTO prompts (prompt, used) VALUES ("Geocaching", false);
+INSERT INTO prompts (prompt, used) VALUES ("There's no place like 127.0.0.1", false);
+INSERT INTO prompts (prompt, used) VALUES ("Minecraft", false);
+INSERT INTO prompts (prompt, used) VALUES ("LARPing", false);
+INSERT INTO prompts (prompt, used) VALUES ("Dungeons and Dragons", false);
+INSERT INTO prompts (prompt, used) VALUES ("Rubik's Cube", false);
+INSERT INTO prompts (prompt, used) VALUES ("Eurogames", false);
+INSERT INTO prompts (prompt, used) VALUES ("Kwisatz Haderach", false);
